10c8518accb1d0e35af5f4a0501bd5f66d7637ec
MarTorres <mar-torres07@hotmail.com>
2023-06-17 17:07:23 -0300

    primer commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mainRoutes = require('./routes/mainRoutes');
+
+app.use(express.static('public'));
+app.set("views", path.join(__dirname, "../views"))
+app.set("view engine", "ejs")
+
+app.listen(3000, () => {
+    console.log('servidor corriendo');
+});
+
+app.use(mainRoutes);
+
